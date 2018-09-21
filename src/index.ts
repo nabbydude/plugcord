b6268947d1404c631b2fe9cbf2826abcf8ea92a7
Riley Froude <nabbydude@gmail.com>
2018-09-21 18:09:27 -0400

    param definitions moved to different packagesStringParam and SubCommandParam moved to plugcord-string-param andplugcord-sub-command-param respectively.

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ export * from "./util-types";
 export * from "./plugin";
 export * from "./command";
 export * from "./param/_";
-export * from "./param/sub-command";
-export * from "./param/string";
