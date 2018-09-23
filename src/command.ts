3ab282eb1f65711561d22f55038ecaa1a64071c6
Riley Froude <nabbydude@gmail.com>
2018-09-23 14:36:51 -0400

    removed unnecessary ts-lint comment

--- src/command.ts
+++ src/command.ts
@@ -2,7 +2,6 @@ import * as Discord from "discord.js";
 import { Param } from "./param";
 import { ParamPayloads } from "./util-types";
 
-// tslint:disable-next-line:max-line-length
 export interface Command<T extends Param[] = Param[]> {
   name: string;
   command: string;
